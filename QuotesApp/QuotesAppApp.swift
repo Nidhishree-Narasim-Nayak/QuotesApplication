52abe74289607a0973f45562b57401069f54aa34
Nidhishree-Narasim-Nayak <nayaknidhishree06@gmail.com>
2025-07-22 23:06:47 +0530

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Nidhishree Nayak on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
